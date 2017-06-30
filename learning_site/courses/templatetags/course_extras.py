a0e13bc460e2641a558ea613a53a570d71da9912
jebin007 <jevinsubedi@gmail.com>
2017-06-30 13:44:23 -0500

    Transform Markdown text into HTML using a custom filter:
    mark_safe(variable) - Marks the variable as being safe to send directly to the browser without escaping or encoding the contents beforehand.

--- learning_site/courses/templatetags/course_extras.py
+++ learning_site/courses/templatetags/course_extras.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
+
+import markdown2
 from courses.models import Course
 
 register = template.Library()
@@ -20,3 +23,10 @@ def time_estimate(word_count):
     '''Estimates the number of minutes it will take to complete a step based on the passed-in wordcontent'''
     minutes = round(word_count/20)
     return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to HTML'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
+
