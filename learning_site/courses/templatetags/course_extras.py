3f23541d0b036e95cf431478c0eff8e1a4b8f384
jebin007 <jevinsubedi@gmail.com>
2017-06-30 04:17:15 -0500

    Complex Template Tags: Return a template from a tag
    Notes:
    Django Documentation Simple Tag vs. Inclusion Tag
    register.inclusion_tag("tag_template.html")(tag_name) or @register.inclusion_tag("tag_template.html") - Registers an inclusion tag. Inclusion tags render a template into wherever they're used.

--- learning_site/courses/templatetags/course_extras.py
+++ learning_site/courses/templatetags/course_extras.py
@@ -1,8 +1,16 @@
 from django import template
 from courses.models import Course
+
 register = template.Library()
 
 @register.simple_tag
 def newest_course():
     ''' Gets the most recent cpurse that was added to the library. '''
     return Course.objects.latest('created_at')
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    '''Returns dictionary of courses to display as navigation pane'''
+    courses = Course.objects.all()
+    return {'courses': courses}
+#We are including a template inside a template unlike including strings on a template. So registering this tag has a different way
