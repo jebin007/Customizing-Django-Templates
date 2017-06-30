ed0ed7c3c025c75bbb05a01c9039801cee14e274
jebin007 <jevinsubedi@gmail.com>
2017-06-30 03:38:05 -0500

    DIY Custom Tags:
    DIY, or Do It Yourself, custom template tags! Build a template tag that will provide you with a link to the newest course in our library at all times.
    
    Notes:
    
    template is Django's module for all things template-related. We'll use this several times in the course.
    
    template.Library is a class that lets us register new tags and filters through an instance of itself.
    
    register.simple_tag(tag_name) or @register.simple_tag - Registers a function as a simple tag. Simple tags don't include new templates, don't have an end tag, and don't assign values to context variables.

---
+++ learning_site/courses/templatetags/course_extras.py
@@ -0,0 +1,8 @@
+from django import template
+from courses.models import Course
+register = template.Library()
+
+@register.simple_tag
+def newest_course():
+    ''' Gets the most recent cpurse that was added to the library. '''
+    return Course.objects.latest('created_at')
